554663d9b31ebf77b8b15519cad4bfed0c2ec6c2
mihir7121 <mihir.nikam1@gmail.com>
2026-04-01 07:56:46 -0700

    fix build

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,9 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhook(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
